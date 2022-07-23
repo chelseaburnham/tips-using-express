87790c005365416a1cdb3e096cbeb13ee385ce20
Chelsea Burnham <chelseaburnham0@gmail.com>
2022-07-22 18:43:23 -0700

    added files from develop folder

---
+++ routes/diagnostics.js
@@ -0,0 +1,37 @@
+const diagnostics = require('express').Router();
+const { response } = require('express');
+const { v4: uuidv4 } = require('uuid');
+const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
+
+// GET Route for retrieving diagnostic information
+diagnostics.get('/', (req, res) => {
+  // TODO: Logic for sending all the content of db/diagnostics.json
+  readFromFile("./db/diagnostics.json").then((data) => res.json(JSON.parse(data)))
+});
+
+// POST Route for a error logging
+diagnostics.post('/', (req, res) => {
+  // TODO: Logic for appending data to the db/diagnostics.json file
+  const {time, errors} = req.body;
+
+  if(time && errors) {
+    const newDiagnostic = {
+      time,
+      errors,
+      error_id: uuidv4()
+    }
+
+    readAndAppend(newDiagnostic, "./db/diagnostics.json")
+
+    const response = {
+      status: 'success',
+      body: newDiagnostic,
+    }
+
+    res.json(response);
+  } else {
+    res.json("Error in diagnostic")
+  }
+});
+
+module.exports = diagnostics;
